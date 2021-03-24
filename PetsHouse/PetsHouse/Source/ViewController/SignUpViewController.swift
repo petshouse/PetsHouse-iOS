f9c0dd9fb66a566e73a8dfdccc3196766408a408
JI-doo <awt0311@gmail.com>
2021-03-24 14:41:28 +0900

    [MODIFY] private

--- PetsHouse/PetsHouse/Source/ViewController/SignUpViewController.swift
+++ PetsHouse/PetsHouse/Source/ViewController/SignUpViewController.swift
@@ -12,42 +12,42 @@ import RxCocoa
 import SnapKit
 import Then
 
-class SignUpViewController: ViewController {
+class SignUpViewController: UIViewController {
     
     //UI
-    let logoView = UIImageView().then {
+    private let logoView = UIImageView().then {
         $0.image = UIImage(named: "Logo with text")
     }
-    let nicknameLbl = UILabel().then {
+    private let nicknameLbl = UILabel().then {
         $0.text = "닉네임"
         $0.textColor = .black
         $0.font = UIFont(name: "BMJUA", size: 15)
     }
-    let emailLbl = UILabel().then {
+    private let emailLbl = UILabel().then {
         $0.text = "이메일"
         $0.textColor = .black
         $0.font = UIFont(name: "BMJUA", size: 15)
     }
-    let passwordLbl = UILabel().then {
+    private let passwordLbl = UILabel().then {
         $0.text = "비밀번호"
         $0.textColor = .black
         $0.font = UIFont(name: "BMJUA", size: 15)
     }
-    let nicknameTxtField = UITextField().then {
+    private let nicknameTxtField = UITextField().then {
         $0.placeholder = "nickname"
         $0.font = UIFont(name: "BMJUA", size: 25)
         $0.layer.cornerRadius = 25
         $0.layer.borderColor = .init(red: 051, green: 051, blue: 051, alpha: 1)
         $0.backgroundColor = .white
     }
-    let emailTxtField = UITextField().then {
+    private  let emailTxtField = UITextField().then {
         $0.placeholder = "email"
         $0.font = UIFont(name: "BMJUA", size: 25)
         $0.layer.cornerRadius = 25
         $0.layer.borderColor = .init(red: 051, green: 051, blue: 051, alpha: 1)
         $0.backgroundColor = .white
     }
-    let checkBtn = UIButton().then {
+    private let checkBtn = UIButton().then {
         $0.clipsToBounds = true
         $0.backgroundColor = .white
         $0.setTitle("중복체크", for: .normal)
@@ -55,21 +55,21 @@ class SignUpViewController: ViewController {
         $0.layer.borderColor = .init(red: 255, green: 204, blue: 153, alpha: 1)
         $0.layer.cornerRadius = 25
     }
-    let passwordTxtField = UITextField().then {
+    private let passwordTxtField = UITextField().then {
         $0.placeholder = "password"
         $0.font = UIFont(name: "BMJUA", size: 25)
         $0.layer.cornerRadius = 25
         $0.layer.borderColor = .init(red: 051, green: 051, blue: 051, alpha: 1)
         $0.backgroundColor = .white
     }
-    let signUpBtn = UIButton().then {
+    private let signUpBtn = UIButton().then {
         $0.clipsToBounds = true
         $0.backgroundColor = .mainColor
         $0.setTitle("회원가입", for: .normal)
         $0.setTitleColor(.white, for: .normal)
         $0.layer.cornerRadius = 25
     }
-    let signInBtn = UIButton().then {
+    private let signInBtn = UIButton().then {
         $0.clipsToBounds = true
         $0.setTitle("이미 계정이 있으신가요? 로그인 하기", for: .normal)
         $0.setTitleColor(.black, for: .normal)
