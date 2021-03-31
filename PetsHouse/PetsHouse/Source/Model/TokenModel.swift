42fe63bef5a5efddec75eb78f80ef758a8e57392
JI-doo <awt0311@gmail.com>
2021-03-31 11:46:30 +0900

    [ADD] token model

---
+++ PetsHouse/PetsHouse/Source/Model/TokenModel.swift
@@ -0,0 +1,15 @@
+//
+//  TokenModel.swift
+//  PetsHouse
+//
+//  Created by 문지수 on 2021/03/31.
+//
+
+import Foundation
+
+struct TokenModel {
+    enum token {
+        case accessToken
+        case refreshToken
+    }
+}
