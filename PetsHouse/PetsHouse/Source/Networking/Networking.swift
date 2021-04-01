efd5a5b7412e925ec7ff1c8c96c539459a15153c
JI-doo <awt0311@gmail.com>
2021-04-01 11:31:59 +0900

    [ADD] networking status

---
+++ PetsHouse/PetsHouse/Source/Networking/Networking.swift
@@ -0,0 +1,17 @@
+//
+//  Networking.swift
+//  PetsHouse
+//
+//  Created by 문지수 on 2021/04/01.
+//
+
+import Foundation
+
+enum Network: Int, Error {
+    case success = 200
+    case ok = 201
+    case forbidden = 403
+    case notFound = 404
+    case conflict = 409
+    
+}
