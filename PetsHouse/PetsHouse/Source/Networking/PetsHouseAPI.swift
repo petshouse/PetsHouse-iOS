1582f3eeeb28e1bf66771b5ede3bc9570500174a
JI-doo <awt0311@gmail.com>
2021-04-07 11:43:15 +0900

    [ADD] verification

--- PetsHouse/PetsHouse/Source/Networking/PetsHouseAPI.swift
+++ PetsHouse/PetsHouse/Source/Networking/PetsHouseAPI.swift
@@ -11,6 +11,7 @@ import Moya
 enum PetsHouseAPI {
     case signIn(_ email: String, _ password: String)
     case signUp(_ nickname: String, _ email: String, _ password: String)
+    case verification(_ code: String)
     case uploadImage(_ image: Data?)
     case loadImage(_ image: Data?)
 }
@@ -26,6 +27,8 @@ extension PetsHouseAPI: TargetType {
             return "/auth"
         case .signUp:
             return "/api/v1/auth "
+        case .verification:
+            return "/api/v1/verification/:code"
         case .uploadImage:
             return "/api/v1/media"
         case .loadImage:
@@ -35,7 +38,7 @@ extension PetsHouseAPI: TargetType {
 
     var method: Moya.Method {
         switch self {
-        case .signIn,.signUp, .uploadImage, .loadImage:
+        case .signIn,.signUp,.verification,.uploadImage, .loadImage:
             return .post
         }
     }
@@ -50,6 +53,8 @@ extension PetsHouseAPI: TargetType {
             return .requestParameters(parameters: ["email": email, "password": password], encoding: JSONEncoding.prettyPrinted)
         case .signUp(let nickname, let email, let password):
             return .requestParameters(parameters: ["nickname": nickname,"email": email, "password": password], encoding: JSONEncoding.prettyPrinted)
+        case .verification(let code):
+            return .requestParameters(parameters: ["code": code], encoding: JSONEncoding.prettyPrinted)
         case .uploadImage(let image):
             return .uploadMultipart([Moya.MultipartFormData(provider: .data(image ?? Data()), name: "image", fileName: "image.jpg", mimeType: "image/jpg")])
         case .loadImage(let image):
@@ -59,7 +64,7 @@ extension PetsHouseAPI: TargetType {
 
     var headers: [String : String]? {
         switch self {
-        case .signIn, .signUp, .uploadImage, .loadImage:
+        case .signIn, .signUp,.verification,.uploadImage, .loadImage:
             return nil
         }
     }
