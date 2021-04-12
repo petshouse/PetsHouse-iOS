3358d0f4e0f120f01bbe3254dcbd402721abd18e
JI-doo <awt0311@gmail.com>
2021-04-12 11:02:30 +0900

    [ADD] write model

---
+++ PetsHouse/PetsHouse/Source/Model/WriteModel.swift
@@ -0,0 +1,15 @@
+//
+//  WriteModel.swift
+//  PetsHouse
+//
+//  Created by 문지수 on 2021/04/12.
+//
+
+import Foundation
+
+
+struct Write: Codable {
+    let title: String
+    let content: String
+    let media: Data?
+}
