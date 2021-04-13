b7cfbd91605162fcae65d4d7ee98ee66f48ebeda
JI-doo <awt0311@gmail.com>
2021-04-13 15:35:46 +0900

    [ADD] sequencePicker

--- PetsHouse/PetsHouse/Source/ViewController/MainViewController.swift
+++ PetsHouse/PetsHouse/Source/ViewController/MainViewController.swift
@@ -14,12 +14,21 @@ import Then
 
 class MainViewController: UIViewController {
     
+    private let disposeBag = DisposeBag()
+    
     private let logoView = UIImageView().then {
         $0.image = UIImage(named: "Logo with text")
     }
     private let sequencePicker = UITextField()
     private let areaPicker = UITextField()
     private let tableView = UITableView()
+    
+    let pickerView = UIPickerView()
+    let doneToolBar = UIToolbar()
+    let doneBarButton = UIBarButtonItem.init(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: nil, action: nil)
+
+    var sequenceData: [String] = []
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +36,36 @@ class MainViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func setUI() {
+        doneBarButton.rx.tap.subscribe(onNext: { _ in
+            let selectRow = self.pickerView.selectedRow(inComponent: 0)
+            self.sequencePicker.text = self.sequenceData[selectRow]
+            self.sequencePicker.resignFirstResponder()
+        }).disposed(by: disposeBag)
+            
+    }
+    
+    func pickerBind() {
+        
+        pickerView.delegate = self
+        pickerView.dataSource = nil
+        sequencePicker.inputView = pickerView
+        
+        _ = Observable.just(sequenceData).bind(to: pickerView.rx.itemTitles) { _, item in return "\(item)" }
 
+    }
+    
+    func createPicker() {
+        doneToolBar.sizeToFit()
+        doneToolBar.items = [doneBarButton]
+        
+        sequencePicker.inputAccessoryView = doneToolBar
+        sequencePicker.inputView = pickerView
+    }
+    
+}
+
+extension MainViewController: UIPickerViewDelegate {
+    
 }
 
