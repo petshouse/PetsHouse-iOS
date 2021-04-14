01a3e1aff39257d6172714bcae046e57802b26d7
JI-doo <awt0311@gmail.com>
2021-04-14 11:22:07 +0900

    [ADD] area picker

--- PetsHouse/PetsHouse/Source/ViewController/MainViewController.swift
+++ PetsHouse/PetsHouse/Source/ViewController/MainViewController.swift
@@ -15,6 +15,9 @@ import Then
 class MainViewController: UIViewController {
     
     private let disposeBag = DisposeBag()
+    private let viewModel = MainViewModel()
+    
+    private let loadData = BehaviorRelay<Void>(value: ())
     
     private let logoView = UIImageView().then {
         $0.image = UIImage(named: "Logo with text")
@@ -28,12 +31,23 @@ class MainViewController: UIViewController {
     let doneBarButton = UIBarButtonItem.init(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: nil, action: nil)
 
     var sequenceData: [String] = []
+    var areaData: [String] = []
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        view.addSubview(logoView)
+        view.addSubview(sequencePicker)
+        view.addSubview(areaPicker)
+        view.addSubview(tableView)
+        
+        setUI()
+        pickerBind()
+        createPicker()
+        constantraint()
     }
     
     func setUI() {
@@ -41,14 +55,20 @@ class MainViewController: UIViewController {
             let selectRow = self.pickerView.selectedRow(inComponent: 0)
             self.sequencePicker.text = self.sequenceData[selectRow]
             self.sequencePicker.resignFirstResponder()
+            self.areaPicker.text = self.areaData[selectRow]
+            self.areaPicker.resignFirstResponder()
         }).disposed(by: disposeBag)
         
+        tableView.delegate = self
+        tableView.register(MainCell.self, forCellReuseIdentifier: "MainCell")
+        tableView.rowHeight = 60
     }
     
     func pickerBind() {
         pickerView.delegate = self
         pickerView.dataSource = nil
         sequencePicker.inputView = pickerView
+        areaPicker.inputView = pickerView
         
         _ = Observable.just(sequenceData).bind(to: pickerView.rx.itemTitles) { _, item in return "\(item)" }
 
@@ -60,6 +80,9 @@ class MainViewController: UIViewController {
         
         sequencePicker.inputAccessoryView = doneToolBar
         sequencePicker.inputView = pickerView
+        
+        areaPicker.inputAccessoryView = doneToolBar
+        areaPicker.inputView = pickerView
     }
     
     private func constantraint() {
