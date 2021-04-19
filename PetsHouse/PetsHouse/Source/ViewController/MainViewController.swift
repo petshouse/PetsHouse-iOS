70a4aecaebcf6d61e6795d3667adc8e9ebf8887a
JI-doo <awt0311@gmail.com>
2021-04-19 10:48:20 +0900

    [ADD] main binding

--- PetsHouse/PetsHouse/Source/ViewController/MainViewController.swift
+++ PetsHouse/PetsHouse/Source/ViewController/MainViewController.swift
@@ -18,9 +18,11 @@ class MainViewController: UIViewController {
     private let viewModel = MainViewModel()
     
     private let loadData = BehaviorRelay<Void>(value: ())
+    var selectIndexPath = PublishRelay<Int>()
+
     
     private let logoView = UIImageView().then {
-        $0.image = UIImage(named: "Logo with text")
+        $0.image = UIImage(named: "Text Logo")
     }
     private let sequencePicker = UITextField()
     private let areaPicker = UITextField()
@@ -48,9 +50,29 @@ class MainViewController: UIViewController {
         pickerBind()
         createPicker()
         constantraint()
+        bindViewModel()
+    }
+    
+    func bindViewModel() {
+        let input = MainViewModel.Input(loadData: loadData.asSignal(onErrorJustReturn: ()))
+        let output = viewModel.transform(input: input)
+        
+        output.loadData.bind(to: tableView.rx.items(cellIdentifier: "mainCell", cellType: MainCell.self)) { row, element, cell in
+            cell.nameLbl.text = element.nickname
+            cell.titleTxtField.text = element.title
+            cell.contentTxtView.text = element.description
+            cell.postImage.image = UIImage(named: element.media)
+            cell.timeLbl.text = element.date
+            cell.sirenBtn.rx.tap.subscribe(onNext: { _ in
+                self.selectIndexPath.accept(row)
+            }).disposed(by: self.disposeBag)
+            
+        }.disposed(by: disposeBag)
     }
     
     func setUI() {
+        let cell = MainCell()
+        
         doneBarButton.rx.tap.subscribe(onNext: { _ in
             let selectRow = self.pickerView.selectedRow(inComponent: 0)
             self.sequencePicker.text = self.sequenceData[selectRow]
@@ -59,36 +81,49 @@ class MainViewController: UIViewController {
             self.areaPicker.resignFirstResponder()
         }).disposed(by: disposeBag)
         
+        cell.moreBtn.rx.tap.subscribe(onNext: { _ in
+            let actionSheet = UIAlertController(title: "부적절한 게시글입니까?", message: "", preferredStyle: .actionSheet)
+            let action = UIAlertAction(title: "신고하기", style: .default, handler: nil)
+            
+            actionSheet.addAction(action)
+            self.present(actionSheet, animated: true, completion: nil)
+        }).disposed(by: self.disposeBag)
+        
         tableView.delegate = self
-        tableView.register(MainCell.self, forCellReuseIdentifier: "MainCell")
-        tableView.rowHeight = 60
+        tableView.dataSource = nil
+        tableView.register(MainCell.self, forCellReuseIdentifier: "mainCell")
+        tableView.rowHeight = 220
     }
+    
+    
     
     func pickerBind() {
         pickerView.delegate = self
         pickerView.dataSource = nil
         sequencePicker.inputView = pickerView
         areaPicker.inputView = pickerView
-        
+
         _ = Observable.just(sequenceData).bind(to: pickerView.rx.itemTitles) { _, item in return "\(item)" }
 
     }
-    
+
     func createPicker() {
         doneToolBar.sizeToFit()
         doneToolBar.items = [doneBarButton]
-        
+
         sequencePicker.inputAccessoryView = doneToolBar
         sequencePicker.inputView = pickerView
-        
+
         areaPicker.inputAccessoryView = doneToolBar
         areaPicker.inputView = pickerView
     }
     
     private func constantraint() {
         logoView.snp.makeConstraints { (make) in
-            make.center.equalTo(view)
-            make.top.equalTo(view.frame.height/3)
+            make.centerX.equalTo(view)
+            make.top.equalTo(view.frame.height/15)
+            make.width.equalTo(170)
+            make.height.equalTo(70)
         }
         sequencePicker.snp.makeConstraints{ (make) in
             make.centerX.equalTo(view)
@@ -114,8 +149,5 @@ class MainViewController: UIViewController {
 }
 
 extension MainViewController: UIPickerViewDelegate, UITableViewDelegate {
- 
-    
-    
 }
 
