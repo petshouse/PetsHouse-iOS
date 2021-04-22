13853639c10369db25596368622e10790d557c5c
JI-doo <awt0311@gmail.com>
2021-04-22 11:48:08 +0900

    [MODIFY] vertification viewmodel

--- PetsHouse/PetsHouse/Source/ViewModel/Auth/VerificationViewModel.swift
+++ PetsHouse/PetsHouse/Source/ViewModel/Auth/VerificationViewModel.swift
@@ -2,7 +2,7 @@
 //  VerificationViewModel.swift
 //  PetsHouse
 //
-//  Created by 문지수 on 2021/04/21.
+//  Created by 문지수 on 2021/04/22.
 //
 
 import Foundation
@@ -15,35 +15,36 @@ class VertificationViewModel: ViewModelType {
     private let disposeBag = DisposeBag()
     
     struct Input {
+        let email: Driver<String>
         let code: Driver<String>
         let doneTap: Driver<Void>
     }
     struct Output {
-        let isEnable: Driver<Bool>
         let result: Signal<String>
+        let isEnable: Driver<Bool>
     }
     
     func transform(input: Input) -> Output {
         let api = Service()
-        let info = input.code
-        let isEnable = info.map { !$0.isEmpty }
+        let info = Driver.combineLatest(input.code, input.email)
+        let isEnable = info.map { !$0.0.isEmpty && !$0.1.isEmpty }
         let result = PublishSubject<String>()
         
-        input.doneTap.asObservable().withLatestFrom(info).subscribe(onNext: { [weak self] code in
-            api.verification(code).subscribe(onNext: { response in
+        input.doneTap.asObservable().withLatestFrom(info).subscribe(onNext: { [weak self] code, email in
+            api.verification(code, email).subscribe(onNext: { response in
                 switch response {
-                case .success:
+                case .ok:
                     result.onCompleted()
                 case .forbidden:
                     result.onNext("forbidden")
                 case .preconditionFailed:
                     result.onNext("preconditionFailed")
                 default:
-                    print("vertification default")
+                    result.onNext("default")
                 }
             }).disposed(by: self!.disposeBag)
         }).disposed(by: disposeBag)
         
-        return Output(isEnable: isEnable.asDriver(), result: result.asSignal(onErrorJustReturn: ""))
+        return Output(result: result.asSignal(onErrorJustReturn: ""), isEnable: isEnable.asDriver())
     }
 }
