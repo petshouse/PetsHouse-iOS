a13e9d4de75301faae028c27697ed0710d35be17
JI-doo <awt0311@gmail.com>
2021-04-22 09:11:36 +0900

    [ADD] vertification viewmodel

---
+++ PetsHouse/PetsHouse/Source/ViewModel/VerificationViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  VerificationViewModel.swift
+//  PetsHouse
+//
+//  Created by 문지수 on 2021/04/21.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+class VertificationViewModel: ViewModelType {
+    
+    private let disposeBag = DisposeBag()
+    
+    struct Input {
+        let email: Driver<String>
+        let doneTap: Driver<Void>
+    }
+    struct Output {
+        let isEnable: Driver<Bool>
+        let result: Signal<String>
+    }
+    
+    func transform(input: Input) -> Output {
+        let api = Service()
+        let info = input.email
+        let isEnable = info.map { !$0.isEmpty }
+        let result = PublishSubject<String>()
+        
+        input.doneTap.asObservable().withLatestFrom(info).subscribe(onNext: { [weak self] email in
+            api.verification(email).subscribe(onNext: { response in
+                switch response {
+                case .success:
+                    result.onCompleted()
+                case .forbidden:
+                    result.onNext("forbidden")
+                case .preconditionFailed:
+                    result.onNext("preconditionFailed")
+                default:
+                    print("vertification default")
+                }
+            }).disposed(by: self!.disposeBag)
+        }).disposed(by: disposeBag)
+        
+        return Output(isEnable: isEnable.asDriver(), result: result.asSignal(onErrorJustReturn: ""))
+    }
+}
