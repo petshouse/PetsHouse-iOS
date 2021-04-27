aad711f4c2944d6c0553a7980d7aae7dc5665407
JI-doo <awt0311@gmail.com>
2021-04-27 13:39:46 +0900

    [ADD] bind viewmodel

--- PetsHouse/PetsHouse/Source/ViewController/Auth/VerificationViewController.swift
+++ PetsHouse/PetsHouse/Source/ViewController/Auth/VerificationViewController.swift
@@ -37,6 +37,9 @@ class VerificationViewController: UIViewController {
         $0.setTitleColor(.white, for: .normal)
         $0.layer.cornerRadius = 15
     }
+    
+    var email: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -48,7 +51,33 @@ class VerificationViewController: UIViewController {
         view.addSubview(nextBtn)
         
         constantraint()
+        bindViewModel()
     }
+    
+    private func bindViewModel() {
+        nextBtn.rx.tap.asObservable().subscribe(onNext: { [unowned self] in
+            let api = Service()
+            api.verification(email, codeTxtField.text!).subscribe(onNext: { (response) in
+                switch response {
+                case .success:
+                    pushData()
+                case .forbidden:
+                    print("올바르지 않은 코드")
+                case .preconditionFailed:
+                    print("preconditionFailed")
+                default:
+                    print("verification default")
+                }
+            }).disposed(by: disposeBag)
+        }).disposed(by: disposeBag)
+    }
+    
+    func pushData() {
+        guard let vc = self.storyboard?.instantiateViewController(withIdentifier: "inputUser") as? SignInViewController else { return }
+        vc.email = email
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+
     
     private func constantraint() {
         textLogo.snp.makeConstraints { (make) in
