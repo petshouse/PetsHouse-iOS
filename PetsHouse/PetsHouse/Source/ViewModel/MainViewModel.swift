9e0a47d567e4834f704be67c7ad2460f2f7523c1
JI-doo <awt0311@gmail.com>
2021-05-30 02:12:37 +0900

    [MODIFY] main viewmodel

--- PetsHouse/PetsHouse/Source/ViewModel/MainViewModel.swift
+++ PetsHouse/PetsHouse/Source/ViewModel/MainViewModel.swift
@@ -15,32 +15,47 @@ class MainViewModel: ViewModelType {
     
     struct Input {
         let loadData: Signal<Void>
+        let loadImage: Signal<Data>
     }
     struct Output {
         let result: Signal<String>
         let loadData: BehaviorRelay<[MainModel]>
+        let loadImage: BehaviorRelay<[loadImageModel]>
     }
     
     func transform(input: Input) -> Output {
         let api = Service()
         let result = PublishSubject<String>()
         let loadData = BehaviorRelay<[MainModel]>(value: [])
-        
-        input.loadData.asObservable()
-            .flatMap{ api.loadPost()}
-            .subscribe(onNext:  { _, response in
+        let loadImage = BehaviorRelay<[loadImageModel]>(value: [])
+
+        input.loadData.asObservable().subscribe(onNext: { _ in
+            api.loadPost().asObservable().subscribe(onNext: { data, response in
                 switch response {
                 case .success:
                     result.onCompleted()
+                    api.loadImage(data?.mediaName ?? " ").subscribe(onNext: { _, response in
+                        switch response {
+                        case .success:
+                            result.onCompleted()
+                        case .forbidden:
+                            result.onNext("forbidden")
+                        case .preconditionFailed:
+                            result.onNext("preconditionFailed")
+                        default:
+                            result.onNext("image default")
+                        }
+                    }).disposed(by: self.disposeBag)
                 case .forbidden:
                     result.onNext("forbidden")
                 case .preconditionFailed:
                     result.onNext("preconditionFailed")
                 default:
-                    print("default")
+                    result.onNext("main default")
                 }
-            }).disposed(by: disposeBag)
+            }).disposed(by: self.disposeBag)
+        }).disposed(by: disposeBag)
         
-        return Output(result: result.asSignal(onErrorJustReturn: ""), loadData: loadData)
+        return Output(result: result.asSignal(onErrorJustReturn: ""), loadData: loadData, loadImage: loadImage)
     }
 }
