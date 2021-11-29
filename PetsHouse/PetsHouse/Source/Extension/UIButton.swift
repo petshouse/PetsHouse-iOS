d5db39a401f279bbbf0665695bc01600782f13fd
JI-doo <awt0311@gmail.com>
2021-11-29 10:32:24 +0900

    [ADD] button extension

---
+++ PetsHouse/PetsHouse/Source/Extension/UIButton.swift
@@ -0,0 +1,20 @@
+//
+//  UIButton.swift
+//  PetsHouse
+//
+//  Created by 문지수 on 2021/11/29.
+//
+
+import UIKit
+
+extension UIButton {
+    func underline() {
+        guard let text = self.titleLabel?.text else { return }
+        let attributedString = NSMutableAttributedString(string: text)
+        //NSAttributedStringKey.foregroundColor : UIColor.blue
+        attributedString.addAttribute(NSAttributedString.Key.underlineColor, value: self.titleColor(for: .normal)!, range: NSRange(location: 0, length: text.count))
+        attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: self.titleColor(for: .normal)!, range: NSRange(location: 0, length: text.count))
+        attributedString.addAttribute(NSAttributedString.Key.underlineStyle, value: NSUnderlineStyle.single.rawValue, range: NSRange(location: 0, length: text.count))
+        self.setAttributedTitle(attributedString, for: .normal)
+    }
+}
