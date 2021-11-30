89b4f4583907e507842bb0625d8a204759b76c8c
JI-doo <awt0311@gmail.com>
2021-11-30 09:18:52 +0900

    [MODIFY] sign in ui

--- PetsHouse/PetsHouse/Source/ViewController/Auth/SignInViewController.swift
+++ PetsHouse/PetsHouse/Source/ViewController/Auth/SignInViewController.swift
@@ -18,8 +18,11 @@ class SignInViewController: UIViewController {
     private let viewModel = SignInViewModel()
     
     //UI
-    private let logoView = UIImageView().then {
-        $0.image = UIImage(named: "Logo with text")
+    private let titleLabel = UILabel().then {
+        $0.text = "이메일과 비밀번호를\n입력하여\n로그인하세요!"
+        $0.textColor = .black
+        $0.font = UIFont(name: "BMJUA", size: 20)
+        $0.numberOfLines = 0
     }
     private let emailLbl = UILabel().then {
         $0.text = "이메일"
@@ -31,18 +34,26 @@ class SignInViewController: UIViewController {
         $0.textColor = .black
         $0.font = UIFont(name: "BMJUA", size: 15)
     }
+    private let emailView = UIView().then {
+        $0.backgroundColor = .textFieldBack
+        $0.layer.cornerRadius = 5
+    }
     private let emailTxtField = UITextField().then {
         $0.placeholder = "email"
         $0.font = UIFont(name: "BMJUA", size: 15)
         $0.autocapitalizationType = .none
         $0.keyboardType = .emailAddress
     }
+    private let passwordView = UIView().then {
+        $0.backgroundColor = .textFieldBack
+        $0.layer.cornerRadius = 5
+    }
     private let passwordTxtField = UITextField().then {
         $0.placeholder = "password"
         $0.font = UIFont(name: "BMJUA", size: 15)
     }
     private let signInBtn = UIButton().then {
-        $0.backgroundColor = .mainColor
+        $0.backgroundColor = .appGray
         $0.setTitle("로그인", for: .normal)
         $0.setTitleColor(.white, for: .normal)
         $0.layer.cornerRadius = 15
@@ -50,7 +61,7 @@ class SignInViewController: UIViewController {
     private let signUpBtn = UIButton().then {
         $0.setTitle("계정이 없으신가요? 회원가입 하기", for: .normal)
         $0.setTitleColor(.black, for: .normal)
-        $0.titleLabel?.font = UIFont(name: "BMJUA", size: 10)
+        $0.titleLabel?.font = UIFont(name: "BMJUA", size: 12)
     }
     
     var email: String = ""
@@ -58,19 +69,14 @@ class SignInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(logoView)
-        view.addSubview(emailLbl)
-        view.addSubview(emailTxtField)
-        view.addSubview(passwordLbl)
-        view.addSubview(passwordTxtField)
-        view.addSubview(signInBtn)
-        view.addSubview(signUpBtn)
+        navigationController?.isNavigationBarHidden = true
         
+        titleLabel.setLineSpacing(lineSpacing: 7.0)
+
         constraint()
         bindViewModel()
         setUI()
     }
-    
     
     func bindViewModel() {
         let input = SignInViewModel.Input(email: emailTxtField.rx.text.orEmpty.asDriver(),
@@ -85,59 +91,74 @@ class SignInViewController: UIViewController {
     }
     
     func setUI() {
-        
-//        navigationController?.isNavigationBarHidden = true
-        
         signUpBtn.rx.tap.subscribe(onNext: { _ in
             self.pushVC("signUpVC")
         }).disposed(by: disposeBag)
         
         passwordTxtField.isSecureTextEntry = true
-
     }
-    
-    
+
     //Constantraint
     func constraint() {
-        logoView.snp.makeConstraints { (make) in
-            make.centerX.equalTo(view)
-            make.top.equalTo(view.frame.height/5)
-            make.width.height.equalTo(220)
+        view.do {
+            $0.addSubview(titleLabel)
+            $0.addSubview(emailLbl)
+            $0.addSubview(emailView)
+            emailView.addSubview(emailTxtField)
+            $0.addSubview(passwordLbl)
+            $0.addSubview(passwordView)
+            passwordView.addSubview(passwordTxtField)
+            $0.addSubview(signInBtn)
+            $0.addSubview(signUpBtn)
         }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
+            make.leading.equalTo(20)
+        }
+
         emailLbl.snp.makeConstraints { (make) in
-            make.top.equalTo(logoView.snp.bottom).offset(80)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-50)
+            make.top.equalTo(titleLabel.snp.bottom).offset(40)
+            make.leading.equalTo(20)
         }
-        emailTxtField.snp.makeConstraints { (make) in
-            make.top.equalTo(emailLbl.snp.bottom).offset(30)
+        emailView.snp.makeConstraints { make in
+            make.top.equalTo(emailLbl.snp.bottom).offset(13)
             make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
-        }
-        passwordLbl.snp.makeConstraints{ (make) in
-            make.top.equalTo(emailTxtField.snp.bottom).offset(50)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
-        }
-        passwordTxtField.snp.makeConstraints { (make) in
-            make.top.equalTo(passwordLbl.snp.bottom).offset(30)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
-        }
-        signInBtn.snp.makeConstraints { (make) in
-            make.top.equalTo(passwordTxtField.snp.bottom).offset(50)
-            make.centerX.equalTo(view)
-            make.leading.equalTo(30)
-            make.trailing.equalTo(-30)
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
             make.height.equalTo(40)
         }
-        signUpBtn.snp.makeConstraints { (make) in
-            make.top.equalTo(signInBtn.snp.bottom).offset(10)
+        emailTxtField.snp.makeConstraints { (make) in
+            make.top.bottom.equalToSuperview().inset(4)
+            make.leading.trailing.equalToSuperview().inset(14)
+        }
+        passwordLbl.snp.makeConstraints{ (make) in
+            make.top.equalTo(emailTxtField.snp.bottom).offset(30)
             make.centerX.equalTo(view)
+            make.leading.equalTo(20)
+        }
+        passwordView.snp.makeConstraints { make in
+            make.top.equalTo(passwordLbl.snp.bottom).offset(13)
+            make.centerX.equalTo(view)
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
+            make.height.equalTo(40)
+        }
+        passwordTxtField.snp.makeConstraints { (make) in
+            make.top.bottom.equalToSuperview().inset(4)
+            make.leading.trailing.equalToSuperview().inset(8)
+        }
+        signInBtn.snp.makeConstraints { (make) in
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-24)
+            make.centerX.equalTo(view)
+            make.leading.equalTo(30)
+            make.trailing.equalTo(-30)
+            make.height.equalTo(45)
+        }
+        signUpBtn.snp.makeConstraints { (make) in
+            make.top.equalTo(passwordTxtField.snp.bottom).offset(10
+            )
+            make.trailing.equalTo(-30)
         }
 
     }
